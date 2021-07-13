6986c34d2310b9df40d80eeda89bb901a641f688
Megha <osr.megha@gmail.com>
2021-07-13 12:56:38 +0530

    MERN CRUD App

--- src/App.js
+++ src/App.js
@@ -1,24 +1,82 @@
-import logo from './logo.svg';
+//import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
+import Axios from "axios";
 import './App.css';
 
 function App() {
+
+  const [foodName, setFoodName] = useState("");
+
+  const [days, setDays] = useState(0);
+
+  const[foodList, setFoodList] = useState([]);
+
+  const[newFoodName, setNewFoodName] = useState('');
+
+  //const [newDays, setNewDays] = useState();
+
+  useEffect(() => {
+    Axios.get("http://localhost:3001/read").then((response)=>{
+      setFoodList(response.data);
+    })
+  }, [])
+   
+  const addToList = () =>{
+    Axios.post("http://localhost:3001/insert", {
+      foodName: foodName,
+      days: days,
+    });
+  };
+
+  const updateFood = (id) =>{
+    Axios.put("http://localhost:3001/update", {
+      id:id,
+      newFoodName: newFoodName,
+      //newDays: newDays,
+    });
+  };
+
+  const deleteFood = (id) =>{
+    Axios.delete(`http://localhost:3001/delete/${id}`);
+  };
+
+
+
+
   return (
+    <React.Fragment>
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <h1> CRUD App with MERN</h1>
+    <label>Food Name</label>
+    <input type="text" onChange={(event) => {setFoodName(event.target.value)}}/>
+    <label>Number of Days Since You Ate</label>
+    <input type="number" onChange={(event) => {setDays(event.target.value)}}/>
+    <button onClick={addToList}>Add to list</button>
+
+    <h1>Food List</h1>
+
+    {foodList.map((val,key) => {
+      return(
+        <div key={key} className="food">
+      <h1>{val.foodName}</h1>
+      <h1>{val.daysSinceIate}</h1>
+
+      <input type="text" placeholder="New Food Name.." onChange={(event) => {setNewFoodName(event.target.value)}}/>
+      
+      <button onClick={()=> updateFood(val._id)}>Update</button>
+
+     {/* <input type="number" placeholder="enter the no. of days" onChange={(event) => {setNewDays(event.target.value)}}/>
+      <button onClick={()=> updateFood(val._id)}>Update</button>*/}
+
+      
+      <button onClick={()=> deleteFood(val._id)}>Delete</button>
+      </div>
+      );
+
+    })}
+
     </div>
+    </React.Fragment>
   );
 }
 
